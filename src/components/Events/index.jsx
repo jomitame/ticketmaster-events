13286db2fe64655f25f5afa5908c6b58d7f5a7f4
jomitame <jomitame@gmail.com>
2024-08-23 17:51:02 -0500

    Passing event like props in a see more button

--- src/components/Events/index.jsx
+++ src/components/Events/index.jsx
@@ -9,12 +9,18 @@ const Events = () => {
     //const events = data._embedded.events;
     const { _embedded: { events }} = data;
 
+    const handleEventItemClick = (id) => {
+        console.log('evento clickeado: ', id);
+    };
+
     const eventsComponent = events.map((eventItem) => (
             <EventItem
                 key={`event-item-${eventItem.id}`}
                 name={eventItem.name}
                 info={eventItem.info}
                 image={eventItem.images[0].url}
+                onEventClick={handleEventItemClick}
+                id={eventItem.id}
             />
         )
     );
