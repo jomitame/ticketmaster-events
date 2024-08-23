c4f17e9cba5ac4731dba15fae0476b398cd514c9
jomitame <jomitame@gmail.com>
2024-08-23 17:38:02 -0500

    Adding event handler to input in navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+
 const Navbar = () => {
+
+    const [search, setSearch] = useState('');
+
+    const handleInputChange = (evt) => {
+        setSearch(evt.target.value);
+    };
+
+    console.log(search);
+
     return (
         <div>
             <p>Mi Boletera</p>
-            <input placeholder="Busca tu evento favorito" />  
+            <input
+                placeholder="Busca tu evento favorito"
+                onChange={handleInputChange}
+                value={search}
+            />  
         </div>
     );
 };
