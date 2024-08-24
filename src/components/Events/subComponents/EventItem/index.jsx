8ee6cb520409a2f0926c4f3570d02e5228d56c0e
jomitame <jomitame@gmail.com>
2024-08-23 21:27:33 -0500

    Refactoring avoiding parent event propagation

--- src/components/Events/subComponents/EventItem/index.jsx
+++ src/components/Events/subComponents/EventItem/index.jsx
@@ -1,13 +1,16 @@
 const EventItem = ({ info, id, name, image, onEventClick }) => {
+
+    const handleSeeMoreClick = (evt) => {
+        evt.stopPropagation();
+        onEventClick(id);
+    }
+
     return (
         <div onClick={() => console.log('parent clicked')}>
             <img src={image} alt={name} width={200} height={200} />
             <h4>{name}</h4>
             <p>{info}</p>
-            <button onClick={(evt) => {
-                evt.stopPropagation();
-                onEventClick(id);
-            }}>Ver mas</button>
+            <button onClick={handleSeeMoreClick}>Ver mas</button>
         </div>
     );
 };
