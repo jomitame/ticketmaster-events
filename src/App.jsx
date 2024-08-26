4f0fdf682eabd894c8476036300beed06c78cf73
jomitame <jomitame@gmail.com>
2024-08-26 09:38:08 -0500

    Formating route for home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,8 @@
-import { useRef, useState } from 'react';
-
-import './App.css'
-import Navbar from './components/Navbar';
-import Events from './components/Events';
-
+import './App.css';
 import Routes from './routes';
 
 
 function App() {
-  /*
-  const [searchTerm, setSearchTerm] = useState('');
-  const containerRef = useRef();
-
-  const handleNavbarSearch = (term) => {
-    console.log(containerRef.current.setSearch(''));
-    setSearchTerm(term);
-  };
-
-  return (
-    <>
-      <Navbar onSearch={handleNavbarSearch} ref={containerRef}/>
-      <Events searchTerm={searchTerm} />
-    </>
-  )
-    */
   return <Routes />
 }
 
--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,3 +1,23 @@
-const Home = () => <div>Home</div>;
+import { useRef, useState } from 'react';
+
+import Navbar from '../../components/Navbar';
+import Events from '../../components/Events';
+
+const Home = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const containerRef = useRef();
+
+    const handleNavbarSearch = (term) => {
+    console.log(containerRef.current.setSearch(''));
+    setSearchTerm(term);
+    };
+
+    return (
+    <>
+        <Navbar onSearch={handleNavbarSearch} ref={containerRef}/>
+        <Events searchTerm={searchTerm} />
+    </>
+    )
+};
 
 export default Home;
