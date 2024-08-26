3eac4b319721c87cb138ef2c9e72d1d8bc612202
jomitame <jomitame@gmail.com>
2024-08-26 09:55:15 -0500

    Definnig custom route 404 error

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,11 +5,13 @@ import {
 
 import Home from '../views/Home';
 import Detail from '../views/Detail';
+import Error404 from '../views/Error404';
 
 const router = createBrowserRouter([
     {
         path: '/',
-        element: <Home />
+        element: <Home />,
+        errorElement: <Error404 />
     },
     {
         path: '/detail',
