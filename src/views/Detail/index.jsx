23e550a44a84510929c9b4f5eca7cefb527eb8b6
jomitame <jomitame@gmail.com>
2024-08-26 12:35:06 -0500

    Accesing to id of events with useParams

--- src/views/Detail/index.jsx
+++ src/views/Detail/index.jsx
@@ -1,4 +1,11 @@
-const Detail = () => <div>Detail</div>;
+import { useParams } from "react-router-dom";
+
+const Detail = () => {
+    const { eventId } = useParams();
+
+    console.dir(eventId);
+    return <div>Detail</div>
+};
 
 export default Detail;
 
