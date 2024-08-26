eeed68f8d71bbe04cde772d2afba78e5862220b1
jomitame <jomitame@gmail.com>
2024-08-26 17:35:46 -0500

    Sending parameters for seraching

--- src/hooks/useEventsData.js
+++ src/hooks/useEventsData.js
@@ -1,31 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 const useEventsData = () => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState();
 
-    useEffect(() => {
-        const fetchEvent = async () => {
-            try{
-                const response = await fetch("https://app.ticketmaster.com/discovery/v2/events.json?apikey=HFXzBJfEfIigZgry3DZeC54c5A5pWzCE");
-                const data = await response.json();
-                setData(data);
-                setIsLoading(false);
-            } catch (error) {
-                setError(error);
-            }
-        };
-
-        // Load API Call
-        fetchEvent();
-
-    }, []);
+    
+    const fetchEvents = async (params) => {
+        try{
+            const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=HFXzBJfEfIigZgry3DZeC54c5A5pWzCE${params?.length ? params : ''}`);
+            const data = await response.json();
+            setData(data);
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+        }
+    };
 
     return {
         events:  data?._embedded?.events || [],
         isLoading,
-        error
+        error,
+        fetchEvents,
     };
 };
 
