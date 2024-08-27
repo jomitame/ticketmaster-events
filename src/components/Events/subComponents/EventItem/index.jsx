0abb4b8ba2440eff723c07907316e63976956ace
jomitame <jomitame@gmail.com>
2024-08-27 17:13:51 -0500

    Adding funtionality to button like/unlike

--- src/components/Events/subComponents/EventItem/index.jsx
+++ src/components/Events/subComponents/EventItem/index.jsx
@@ -1,9 +1,12 @@
 import styles from './EventItem.module.css';
 
+import useLikedEvents from '../../../../hooks/useLikeEvents'
 import HearthFilled from '../../../../assets/hearth-filled.png'
 import HearthUnFilled from '../../../../assets/hearth-unfilled.png'
 
 const EventItem = ({ info, id, name, image, onEventClick }) => {
+
+    const { isEventLiked, toggleEventLike } = useLikedEvents(id);
 
     const handleSeeMoreClick = (evt) => {
         evt.stopPropagation();
@@ -11,13 +14,13 @@ const EventItem = ({ info, id, name, image, onEventClick }) => {
     };
 
     const handleHerathClick = () => {
-        
+        toggleEventLike();
     };
 
     return (
         <div onClick={() => console.log('parent clicked')} className={`${styles.eventItemContainer} ${styles.anotherContainer}`}>
             <div className={styles.imageContainer}>
-                <img src={HearthUnFilled} alt="Hearth button" className={styles.hearthImage} onClick={handleHerathClick}/>
+                <img src={isEventLiked ? HearthFilled : HearthUnFilled} alt="Hearth button" className={styles.hearthImage} onClick={handleHerathClick}/>
                 <img src={image} alt={name} width={200} height={200} />
             </div>
             <div className={styles.eventInfoContainer}>
