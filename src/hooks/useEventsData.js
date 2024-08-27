2727dffb420767e8a1a0ded37d6e655ad6b94ff5
jomitame <jomitame@gmail.com>
2024-08-27 12:47:37 -0500

    Adaptin hook for local state and stora for save values in global way

--- src/hooks/useEventsData.js
+++ src/hooks/useEventsData.js
@@ -1,8 +1,23 @@
-import useEventsResults from '../state/events-results';
+import { useState } from 'react';
 
+// Hook for API call and save it in local state
 const useEventsData = () => {
-    const { data, isLoading, error, fetchEvents } = useEventsResults();
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState();
+
     
+    const fetchEvents = async (params) => {
+        try{
+            const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${import.meta.env.VITE_TICKET_MASTER_API_KEY}${params?.length ? params : ''}`);
+            const data = await response.json();
+            setData(data);
+            setIsLoading(false);
+        } catch (error) {
+            setError(error);
+        }
+    };
+
     return {
         events:  data?._embedded?.events || [],
         page: data?.page || {},
--- src/state/events-results.js
+++ src/state/events-results.js
@@ -1,16 +1,17 @@
 import { create } from "zustand";
 
+// Store to save values in global way
 const useEventsResults = create((set) => ({
     data: [],
     error: null,
     isLoading: false,
     fetchEvents: async (params) => {
         try{
-
             await set(() => ({ isLoading: true }));
 
             const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events.json?apikey=${import.meta.env.VITE_TICKET_MASTER_API_KEY}${params?.length ? params : ''}`);
             const data = await response.json();
+            
             await set(() => ({ data, isLoading: false}));
         } catch (error) {
             await set(() => ({ error }));
