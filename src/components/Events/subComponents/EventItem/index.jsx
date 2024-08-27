554fce1d63cc6778315a83c90abb248fafa82e80
jomitame <jomitame@gmail.com>
2024-08-27 15:50:43 -0500

    Addng like and unlike button to events

--- src/components/Events/subComponents/EventItem/index.jsx
+++ src/components/Events/subComponents/EventItem/index.jsx
@@ -1,15 +1,25 @@
 import styles from './EventItem.module.css';
 
+import HearthFilled from '../../../../assets/hearth-filled.png'
+import HearthUnFilled from '../../../../assets/hearth-unfilled.png'
+
 const EventItem = ({ info, id, name, image, onEventClick }) => {
 
     const handleSeeMoreClick = (evt) => {
         evt.stopPropagation();
         onEventClick(id);
-    }
+    };
+
+    const handleHerathClick = () => {
+        
+    };
 
     return (
-        <div onClick={() => console.log('parent clicked')} className={styles.eventItemContainer}>
-            <img src={image} alt={name} width={200} height={200} />
+        <div onClick={() => console.log('parent clicked')} className={`${styles.eventItemContainer} ${styles.anotherContainer}`}>
+            <div className={styles.imageContainer}>
+                <img src={HearthUnFilled} alt="Hearth button" className={styles.hearthImage} onClick={handleHerathClick}/>
+                <img src={image} alt={name} width={200} height={200} />
+            </div>
             <div className={styles.eventInfoContainer}>
                 <h4 className={styles.eventName}>{name}</h4>
                 <p className={styles.eventInfo}>{info}</p>
