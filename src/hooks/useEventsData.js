24fb60c420476dc9a1c8753fa5e3e6a056b924da
jomitame <jomitame@gmail.com>
2024-08-26 19:04:00 -0500

    Ading pagination for result of events

--- src/hooks/useEventsData.js
+++ src/hooks/useEventsData.js
@@ -19,6 +19,7 @@ const useEventsData = () => {
 
     return {
         events:  data?._embedded?.events || [],
+        page: data?.page || {},
         isLoading,
         error,
         fetchEvents,
