992ad84c583dff6b59af1b56e3ea6765b8d63816
jomitame <jomitame@gmail.com>
2024-08-27 18:23:14 -0500

    Creating tab for profile

--- src/views/Profile/index.jsx
+++ src/views/Profile/index.jsx
@@ -1,10 +1,32 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+
+import styles from './Profile.module.css';
 
 
 const Profile = () => {
+    const { pathname } = useLocation();
+    const navigate = useNavigate();
+
+    const handleTabClick = (path) => {
+        navigate(`/profile/${path}`);
+    };
+
     return (
         <div>
-            Profile
+            <div>
+                <span
+                    className={`${pathname.includes('my-info') ? styles.active : ''}`}
+                    onClick={() => handleTabClick('my-info')}
+                >
+                        My Info
+                </span>
+                <span
+                    className={`${pathname.includes('liked-events') ? styles.active : ''}`}
+                    onClick={() => handleTabClick('liked-events')}
+                >
+                        Favorites
+                </span>
+            </div>
             <Outlet />
         </div>
     );};
