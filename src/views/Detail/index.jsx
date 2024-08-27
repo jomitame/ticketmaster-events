6963f6ec91088e011b6e2934c08335efb36a9620
jomitame <jomitame@gmail.com>
2024-08-26 19:26:13 -0500

    Gettin details from API

--- src/views/Detail/index.jsx
+++ src/views/Detail/index.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Detail = () => {
     const { eventId } = useParams();
+    const [eventData, setEventData] = useState({});
+    const [error, setError] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
 
-    console.dir(eventId);
+    useEffect(() => {
+        const fetchEventData = async () => {
+            try {
+                const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events/${eventId}?apikey=HFXzBJfEfIigZgry3DZeC54c5A5pWzCE`);
+                const data = await response.json()
+
+                setEventData(data);
+                setIsLoading(false);
+            } catch (error) {
+                setEventData({});
+                setError(error);
+                setIsLoading(false);
+            }
+        };
+
+        fetchEventData();
+    }, []);
+
+    console.log(eventData);
     return <div>Detail</div>
 };
 
