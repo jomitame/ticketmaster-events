f3ed0b0f59edb767729fccbca53d1bc9b3911cd2
jomitame <jomitame@gmail.com>
2024-08-27 19:31:43 -0500

    Creating a form for my info profile

--- src/views/Profile/components/MyInfo/index.jsx
+++ src/views/Profile/components/MyInfo/index.jsx
@@ -1,8 +1,59 @@
+import { useForm } from "react-hook-form";
+
+import styles from "./MyInfo.module.css";
+import { useEffect } from "react";
+
+const USER_DATA = 'userData';
+
 const MyInfo = () => {
+    const { handleSubmit, register, setValue} = useForm();
+
+    useEffect(() => {
+        try {
+            const userData = JSON.parse(localStorage.getItem(USER_DATA)) || [];
+            setValue('name', userData?.name);
+            setValue('email', userData?.email);
+            setValue('age', userData?.age);
+        } catch (error) {
+            console.error(error);
+        }
+    }, []);
+
+    const handleFormSubmit = (data) => {
+        try {
+            localStorage.setItem(USER_DATA, JSON.stringify(data));
+            alert("User Updated");
+        } catch (error) {
+            alert("ERROR");
+        }
+    };
+
     return (
-        <div>
-            My Info
-        </div>
+        <form onSubmit={handleSubmit(handleFormSubmit)} className={styles.form}>
+            <label className={styles.label}>
+                Name
+                <input
+                    {...register('name', { required: true, minLength:1, maxLength: 120 })}
+                    className={styles.input}
+                />
+            </label>
+            <label className={styles.label}>
+                Email
+                <input
+                    {...register('email', { required: true, min: 18, max: 200})}
+                    className={styles.input}
+                />
+            </label>
+            <label className={styles.label}>
+                Age
+                <input
+                    {...register('age', { required: true, min: 18, max: 120, valueAsNumber: true})}
+                    className={styles.input}
+                    type="number"
+                />
+            </label>
+            <button type="submit" className={styles.submitButton}>Save</button>
+        </form>
     );
 };
 
