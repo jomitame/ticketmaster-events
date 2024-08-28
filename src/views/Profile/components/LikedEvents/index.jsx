f66094f2206e6180d2c2a535a452391db5b505ae
jomitame <jomitame@gmail.com>
2024-08-28 04:52:56 -0500

    Adding functinality to favorites events

--- src/views/Profile/components/LikedEvents/index.jsx
+++ src/views/Profile/components/LikedEvents/index.jsx
@@ -1,7 +1,66 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { LIKED_EVENTS_STORAGE_KEY } from '../../../../utils/constants';
+import EventItem from '../../../../components/Events/subComponents/EventItem';
+
 const LikedEvents = () =>  {
+    const [events, setEvents] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState({});
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchEventsDetails = async () => {
+            try {
+                setIsLoading(true);
+                const LikedEvents = JSON.parse(localStorage.getItem(LIKED_EVENTS_STORAGE_KEY)) || [];
+                
+                const result = [];
+
+                for (const eventId of LikedEvents) {
+                    const response = await fetch(`https://app.ticketmaster.com/discovery/v2/events/${eventId}?apikey=${import.meta.env.VITE_TICKET_MASTER_API_KEY}`);
+                    const data = await response.json();
+
+                    result.push(data);
+                }
+                
+                setEvents(result);
+
+            } catch (error) {
+                setError(error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchEventsDetails();
+    }, []);
+
+    const handleEventItemClick = (eventId) => {
+        navigate(`/detail/${eventId}`);
+    };
+
+    if (Object.keys(error).length > 0) {
+        return <div>Somthing wrong happens...</div>
+    }
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+
     return (
         <div>
-            Liked Events
+            {events.map((event, index) => (
+                <EventItem
+                    key={`liked-event-item-${event.id}-${index}`}
+                    name={event.name}
+                    info={event.info}
+                    image={event.images[0].url}
+                    onEventClick={handleEventItemClick}
+                    id={event.id}
+                />
+            ))}
         </div>
     );
 };
