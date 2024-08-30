496a94fbf88e3aef0d06d9f23e06a4a0e7e2a0a1
jomitame <jomitame@gmail.com>
2024-08-29 19:18:15 -0500

    moization of function

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState, useCallback } from 'react';
 import ReactPaginate from 'react-paginate';
 
 import Navbar from '../../components/Navbar';
@@ -24,9 +24,9 @@ const Home = () => {
         fetchEvents(`&keyword=${term}`);
     };
 
-    const handlePageClick = ({ selected }) => {
+    const handlePageClick = useCallback(({ selected }) => {
         fetchEvents(`&keyword=${searchTerm}&page=${selected}`)
-    };
+    }, [searchTerm, fetchEvents]);
 
     const renderEvents = () => {
         if (isLoading) {
