d0a03464c83b10e264e6c136a71fbc4df6c76688
jomitame <jomitame@gmail.com>
2024-08-29 19:24:10 -0500

    Implementin useMemo to cache calculation to avoid recalculation

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState, useCallback } from 'react';
+import { useEffect, useRef, useState, useCallback, useMemo } from 'react';
 import ReactPaginate from 'react-paginate';
 
 import Navbar from '../../components/Navbar';
@@ -8,8 +8,8 @@ import styles from './Home.module.css';
 
 const Home = () => {
     const { data, isLoading, error, fetchEvents } = useEventsResults();
-    const events = data?._embedded?.events || [];
-    const page = data?.page || {}
+    const events = useMemo(data?._embedded?.events || [], [data?._embedded?.events]);
+    const page = useMemo(data?.page || {}, [data?.page]);
     const [isToggle, setIsToggle] = useState(false);
     
     const [searchTerm, setSearchTerm] = useState('');
